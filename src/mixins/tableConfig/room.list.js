dd63d498d5271a2c4895a0a0098677f157a2941b
vina <vina.liu@zenlayer.com>
2021-03-30 16:27:37 +0800

    改造全部的table组件

--- src/mixins/tableConfig/room.list.js
+++ src/mixins/tableConfig/room.list.js
@@ -2,77 +2,71 @@ import moment from 'moment'
 export const listTableMixin = {
     data() {
         return {
-            tableColumns: [
-            {
-                    dataIndex: 'code',
-                    title: '编号',
-            },
-            {
-                dataIndex: 'name',
-                title: '名称',
-            }, {
-                
-                dataIndex: 'style',
-                title: '风格',
-            }, {
-                dataIndex: 'maxPeople',
-                title: '最多人数',
-                sorter: true
-            },
-            {
-                dataIndex: 'status',
-                title: 'status',
-                width: '100px'
-            },
-            {
-                dataIndex:'createTime',
-                title:"创建时间",
-                width:'200px'
-            },
-            {
-                dataIndex: 'action',
-                title: 'action',
-                width: "140px"
-            }],
-            detailColumns: [
-                {
-
-                    type: "text",
-                    props: "status",
-                    mapping: [{
-                            text: '新建',
-                            value: '0',
-                        },
-                        {
-                            text: '装修中',
-                            value: '1',
-                        }, {
-                            text: '可使用',
-                            value: '2',
-                        }, {
-                            text: '暂停使用',
-                            value: '3',
-                        }, {
-                            text: '下线',
-                            value: '4',
-                        },
-                    ]
-                
+            tableConfig: {
+                scroll: {
+                    x: 900
+                },
+                disableFlex: true,
+                columns: [
+                    {
+                        props: 'code',
+                        title: '编号',
                 },
                 {
-                    "props": "createTime",
-                    renderContent: ({ value, dataSource }) => {
-                        console.log("dataSource",dataSource)
-                        
-                    return dataSource.createTime
+                    props: 'name',
+                    title: '名称',
+                }, {
+                    
+                    props: 'style',
+                    title: '风格',
+                }, {
+                    props: 'maxPeople',
+                    title: '最多人数',
+                    sorter: true
+                },
+                {
+                    props: 'status',
+                    title: 'status',
+                    width: '100px',
+                    mapping: [{
+                        text: '新建',
+                        value: '0',
+                    },
+                    {
+                        text: '装修中',
+                        value: '1',
+                    }, {
+                        text: '可使用',
+                        value: '2',
+                    }, {
+                        text: '暂停使用',
+                        value: '3',
+                    }, {
+                        text: '下线',
+                        value: '4',
+                    },
+                ]
+                },
+                {
+                    props:'createTime',
+                    title:"创建时间",
+                    width:'200px',
+                    type: 'render',
+                    renderContent: ({
+                        record
+                    }) => {
+                        return record.createTime
                     ? moment
-                            .parseZone(dataSource.createTime)
+                            .parseZone(record.createTime)
                             .local()
                             .format('YYYY-MM-DD HH:mm:ss')
                     : ''
                     },
                 },
-            ],
+
+                ]
+            },
+           
         }
     }
 }
