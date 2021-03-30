35e863138ee73207ca7707676f8d3a219de6f7c5
vina <vina.liu@zenlayer.com>
2021-03-30 20:08:56 +0800

    table1

--- src/views/Merchant/room.vue
+++ src/views/Merchant/room.vue
@@ -1,6 +1,12 @@
 <template>
 	<div>
 		<div class="zen-table-content">
+			<div class="OptionItem">
+                <a-button type="primary" class="addItem" @click="addHandle">
+				  创建标签
+			   </a-button>
+			 
+			 </div>
 			 	<ztable
 				:tableConfig="tableConfig"
 				:dataSource="dataSource"
@@ -8,15 +14,26 @@
 				:pagination="pagination"
 				@change="tableChangeHandle"
 			></ztable>
+			  <zmodal
+				title="创建房间"
+				:visible="visibleAddModal"
+				@handleOk="zmodalHandleOk"
+				@handleCancel="zmodalHandleCancel"
+				:destroyOnClose="true"
+				size="small"
+				:formConfig="zmodalFormConfig"
+		  >
+			</zmodal>
 		</div>
 	</div>
 </template>
 <script>
 import { tableConfigMixin } from '@/mixins/tableConfig/index.js'
 import { listTableMixin } from '@/mixins/tableConfig/room.list.js'
+import { addFormMixin } from '@/mixins/formConfig/room.add.js'
 import axios from '@/axios'
 export default {
-	mixins: [tableConfigMixin, listTableMixin],
+	mixins: [tableConfigMixin, listTableMixin,addFormMixin],
 	data() {
 		return {
 			dataSource: [],
@@ -49,6 +66,38 @@ export default {
 		init() {
 			this.getTableDatas()
 		},
+		addHandle(){
+            this.visibleAddModal=true
+		},
+		zmodalHandleCancel(){
+            this.visibleAddModal=false
+		},
+		// 创建员工
+		zmodalHandleOk(e, values) {
+			console.log(values)
+			let arr=[]
+			values.style.forEach((res)=>{
+                arr.push(res.key)
+			})
+			console.log("arr",arr.toString())
+			axios
+				.post(`/api/res/room/create`, {
+					merchantUid: localStorage.getItem("uid"),
+					maxPeople:values.maxPeople,
+					name:values.name,
+					status:values.status,
+					style:arr.toString()
+					
+				})
+				.then((res) => {
+					this.$message.success('Successful')
+					this.visibleAddModal=false
+					this.getTableDatas()
+				})
+				.finally((res) => {
+					this.visibleAddModal=false
+				})
+		},
 	},
 	mounted() {
 		this.init()
@@ -65,5 +114,13 @@ export default {
 .operationsListActive li:hover {
 	cursor: pointer;
 	background-color: color(~`colorPalette('@{brand1-6}', 1) `);
+}
+.OptionItem{
+	 display: flex;
+	 justify-content:flex-end;
+	 padding: 10px 0px;
+}
+.addItem{
+
 }
 </style>
